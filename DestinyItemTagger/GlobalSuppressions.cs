fdee3c0f436d18047211c9720191986270ecd0c2
Matthew Castle <matthew.castle@live.co.uk>
2019-07-27 18:41:27 +0100

    Last code anaysis tidy

--- DestinyItemTagger/GlobalSuppressions.cs
+++ DestinyItemTagger/GlobalSuppressions.cs
@@ -1,6 +1,4 @@
 ﻿// <copyright file="GlobalSuppressions.cs" company="PlaceholderCompany">
 // Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "type", Target = "~T:DestinyItemTagger.Program")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "<Pending>", Scope = "member", Target = "~M:DestinyItemTagger.Main.#ctor")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1305:Specify IFormatProvider", Justification = "<Pending>", Scope = "member", Target = "~M:DestinyItemTagger.Program.CheckArmourForRecomendedPerks(System.String[]@,System.String[][])")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1305:Specify IFormatProvider", Justification = "UK Use Only", Scope = "member", Target = "~M:DestinyItemTagger.Program.CheckArmourForRecomendedPerks(System.String[]@,System.String[][])")]
